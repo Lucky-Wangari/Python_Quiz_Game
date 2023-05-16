0ef9fb94e339aad0b87d9aa568454c92900ae8b4
Lucky-Wangari <wangarimarylucky>
2023-05-16 21:36:23 +0300

    added lower case

--- questions.py
+++ questions.py
@@ -9,68 +9,68 @@ print("Welcome to speed quiz")
 do_quiz = input("Ready to start playing?  ")
 # the input function allows the user to type something
 # the space after the closing quotes is to give room for the user to input the answer
-if do_quiz != "Yes":
+if do_quiz.lower() != "yes":
    quit()
    
 print("Lets start playing")
 
 # If its correct the system will proceed to print
 do_quiz = input("What are .py files?  ").lower()
-if do_quiz == "py files are Python source files":
+if do_quiz.lower() == "py files are python source files":
     print("Correct!")
 else:
     print("Incorect!")
 
 do_quiz = input("Define String in Python? ")
-if do_quiz == "String in Python is formed using a sequence of characters":
+if do_quiz.lower() == "string in python is formed using a sequence of characters":
     print("Correct!")
 else:
     print("Incorect!")
     
 do_quiz = input("Are strings mutable or immutable?  ")
-if do_quiz == "Python strings are immutable":
+if do_quiz.lower() == "python strings are immutable":
     print("Correct!")
 else:
     print("Incorect!")
     
-do_quiz = input("How are strings denaoted in python?  ")
-if do_quiz == "Using double or single quotes":
+do_quiz = input("How are strings denoted in python?  ")
+if do_quiz.lower() == "using double or single quotes":
     print("Correct!")
 else:
     print("Incorect!")
     
 do_quiz = input("What is namespace in Python?  ")
-if do_quiz == "A system that provides unique names for objects":
+if do_quiz.lower() == "a system that provides unique names for objects":
     print("Correct!")
 else:
     print("Incorect!")
     
 do_quiz = input("What is OOP?  ")
-if do_quiz == "Objects Oriented Programming":
+if do_quiz.lower() == "objects oriented programming":
     print("Correct!")
 else:
     print("Incorect!")
     
 do_quiz = input("Which command is used to delete files in Python?  ")
-if do_quiz == "x.remove(filename)":
+if do_quiz.lower() == "x.remove(filename)":
     print("Correct!")
 else:
     print("Incorect!")
     
 do_quiz = input("What is append used for in Python?  ")
-if do_quiz == "It is used to add elements at the end of a list":
+if do_quiz == "it is used to add elements at the end of a list":
     print("Correct!")
 else:
     print("Incorect!")
     
 do_quiz = input("What is a boolean in Python?  ")
-if do_quiz == "It contains two values, which are true and false":
+if do_quiz.lower() == "it contains two values, which are true and false":
     print("Correct!")
 else:
     print("Incorect!")
     
 do_quiz = input("What are the functions in Python?  ")
-if do_quiz == "A block of code that is executable only when it is called":
+if do_quiz.lower() == "a block of code that is executable only when it is called":
     print("Correct!")
 else:
     print("Incorect!")
